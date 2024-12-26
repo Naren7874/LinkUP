937ceb6d3d25e9b0fa5271e2cdfd7936f50e3439
Narendra <malinarendra7874@gmail.com>
2024-12-26 23:07:17 +0530

    add to backend

--- thread_ui/vite.config.js
+++ thread_ui/vite.config.js
@@ -6,14 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port: 3000,
-    //get rid of the CORS err 
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,  // allow proxying HTTPS requests to the server
-      }
-    }
   }
 
 })
